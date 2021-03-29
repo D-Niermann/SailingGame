ba3144c5b3e9ed357fc1c0935da0ba8904402954
mayonnace <81087663+mayonnace@users.noreply.github.com>
2021-03-29 21:30:19 +0300

    Create economy.gd

---
+++ economy.gd
@@ -0,0 +1,96 @@
+extends Node
+
+
+# Declare member variables here.
+var money: float = 100
+const dropPower: float = 0.125 # bigger values make changes in price due to abundance of an item more drastical
+const minCurrency: float = 1.0 # smallest possible money unit, like one cent
+var malls: Dictionary = {
+	"bananaTown": {
+		"capacity": Vector2(0, 100),
+		"consumption": 0,
+		"grows": {"banana": 2.0},
+		"goods": {"banana": 10, "apple": INF},
+		"money": 1000
+	},
+	"appleCity": {
+		"capacity": Vector2(0, 100),
+		"consumption": 0,
+		"grows": {"apple": 1.0},
+		"goods": {},
+		"money": 1000
+	}
+}
+var goods: Dictionary = {
+	"banana": {"price": 10, "weight": 1.0, "res": "res://exampleItem.tscn", "icon": "res://icon.png"},
+	"apple": {"price": 10, "weight": 1.0, "res": "res://exampleItem.tscn", "icon": "res://icon.png"}
+}
+var cases: Dictionary = {
+	"storm": {
+		"banana": 0.5
+	},
+	"fire": {
+		"apple": 0.5
+	}
+}
+
+
+# Returns icon for the given product.
+func getIcon(of: String):
+	return goods[of]["icon"]
+
+
+# Returns weight for the given product.
+func getWeight(of: String):
+	return goods[of]["weight"]
+
+
+# Returns price for the given product at the given place.
+func getPrice(of: String, at: String):
+	if !malls[at]["goods"].has(of):
+		return 0
+	else:
+		var price: float = goods[of]["price"]
+		var amount = getAmount(of, at)
+		if amount != INF:
+			price = price / max(1.0, pow(getAmount(of, at), dropPower))
+		return max(minCurrency, price - fmod(price, minCurrency))
+
+
+# Returns amount for the given product at the given place.
+func getAmount(of: String, at: String):
+	if !malls[at]["goods"].has(of):
+		return 0
+	else:
+		return floor(malls[at]["goods"][of])
+
+
+# Adds grows to goods, and consumes some.
+func cycle():
+	for mallKey in malls.keys():
+		var total: float = 0.0
+		var mall: Dictionary = malls[mallKey]
+		var canStoreMore: bool = mall["capacity"].x < mall["capacity"].y
+		for goodKey in mall["goods"].keys():
+			# production
+			if canStoreMore && mall["grows"].has(goodKey):
+				mall["goods"][goodKey] += mall["grows"][goodKey]
+			# consumption
+			mall["goods"][goodKey] -= mall["consumption"]
+			if mall["goods"][goodKey] <= 0:
+				mall["goods"].erase(goodKey)
+			else:
+				total += mall["goods"][goodKey]
+		mall["capacity"].x = total
+
+
+# Runs a case on the given locations.
+func runCase(case: String, at: PoolStringArray):
+	var changes: Dictionary = cases[case]
+	for mall in at:
+		var current: Dictionary = malls[mall]["goods"]
+		for key in changes.keys():
+			if !current.has(key):
+				continue
+			var newAmount = current[key] * changes[key]
+			newAmount -= fmod(newAmount, goods[key])
