048d1059be1287b41cf55388d9d057f86cef7dbc
mayonnace <81087663+mayonnace@users.noreply.github.com>
2021-04-18 23:27:21 +0300

    Tried to add some core functions.
    I didn't use your functions, but didn't delete them either.

--- ControlNodes/CrewManager/CrewManager.gd
+++ ControlNodes/CrewManager/CrewManager.gd
@@ -9,17 +9,125 @@ const TG_UTILITY = "tg-utility"
 const TG_RELAX = "tg-relax"
 
 var myShip # parent ship of this manager
-var humanNodes # list of all humans on ship (refs)
 
-var currentAssignments = {TG_RELAX: [], TG_WEAPONS: [], TG_UTILITY: [], TG_NAVIGATION: []}
+var currentAssignments = {TG_RELAX: [], TG_WEAPONS: [], TG_UTILITY: [], TG_NAVIGATION: []} # crewIDs
+var crew = {} # crewID: {ref (referenceOfTheCrew), type (typeOfTheCrew), job (jobID), inventory, tired, xform (localTransform),..}
+var jobs = {} # jobID: {ref (referenceOfTheItem), name (nameOfTheJob), crew (crewID), refName (refID in baseItem for loadGame)}
+var jobSpecs = {} # nameOfTheJob: {type (typeOfTheJob), priority (priorityOfTheJob)}
+
+const HUMAN: PackedScene = preload("res://ObjectNodes/Human/Human.tscn")
 
 func _ready():
 	myShip = get_parent()
-	humanNodes = get_children()
-	
-func _physics_process(delta):
-	checkMen()
+	repopulate()
 
+func _physics_process(delta):
+	checkAndUpdate()
+	matchJobsToCrew()
+
+func repopulate():
+	"""
+	Deletes all children, iterates over crew data, and recreates their instances.
+	"""
+	for child in get_children():
+		child.queue_free()
+	for crewID in crew.keys():
+		var crewSpec = crew[crewID]
+		var newHuman = HUMAN.instance()
+		add_child(newHuman)
+		newHuman.transform = crewSpec["xform"]
+
+func checkAndUpdate():
+	"""
+	Iterates over crew members, checks their status, runs their AI for pathfinding etc., and updates their locations.
+	"""
+	for crewID in crew.keys():
+		var crewSpec = crew[crewID]
+		#???
+
+func matchJobsToCrew():
+	"""
+	Tries to match existing but not picked jobs, to the closest crew member of that job type.
+	"""
+	for key in jobs.keys():
+		var job = jobs[key]
+		var crewID = job["crew"]
+		if crewID != null: # this job is already getting done by someone
+			continue
+		var jobSpec = jobSpecs[job["name"]]
+		var jobType = jobSpec["type"]
+		var jobPriority = jobSpec["priority"]
+		var jobReference = job["ref"]
+		var closest = null
+		var distance = 999999999
+		for crewIDTwo in currentAssignments[jobType]:
+			var crewSpec = crew[crewIDTwo]
+			var crewJob = crewSpec["job"]
+			var crewPriority = jobSpecs[jobs[crewJob]["name"]]["priority"]
+			if crewJob != null && crewPriority <= jobPriority: # this crew is already getting done some job with equal or less priority
+				continue
+			var crewReference = crewSpec["ref"]
+			var tempDist = jobReference.transform.origin.distance_squared_to(crewReference.transform.origin) # may want to use path length instead
+			if tempDist < distance:
+				distance = tempDist
+				closest = crewIDTwo
+		if closest != null: # means a suitable crew member is found to do the job
+			var crewSpec = crew[closest]
+			var crewOldJob = crewSpec["job"]
+			jobs[crewOldJob]["crew"] = null
+			crewSpec["job"] = key
+			job["crew"] = closest
+			break # only one job gets matched in a physics frame, to prevent performance issues
+
+func removeJob(jobID):
+	"""
+	Removes an existing job from the list.
+	"""
+	crew[jobs[jobID]["crew"]]["job"] = null
+	jobs.erase(jobID)
+
+func removeCrew(crewID):
+	"""
+	Deletes an existing crew.
+	"""
+	jobs[crew[crewID]["job"]]["crew"] = null
+	crew.erase(crewID)
+	crew.queue_free()
+
+func insertJob(nameOfTheJob, reference):
+	"""
+	Puts a new job on the list.
+	"""
+	var ID = 0
+	while jobs.has(ID):
+		ID += 1
+	jobs[ID] = {"crew": null, "name": nameOfTheJob, "ref": reference}
+
+func insertCrew(reference):
+	"""
+	Creates new crew on board.
+	"""
+	var ID = 0
+	while crew.has(ID):
+		ID += 1
+	crew[ID] = {"job": null, "type": TG_RELAX, "ref": reference, "xform": Transform.IDENTITY}
+	currentAssignments[TG_RELAX].append(ID)
+	var newHuman = HUMAN.instance()
+	add_child(newHuman)
+	newHuman.transform = crew[ID]["xform"]
+
+func changeType(crewID, newType):
+	"""
+	Changes current assignment of the given crew to the given type.
+	"""
+	var oldType = crew[crewID]["type"]
+	if oldType != newType:
+		currentAssignments[oldType].erase(crewID)
+		currentAssignments[newType].append(crewID)
+		var oldJob = crew[crewID]["job"]
+		if oldJob != null && jobSpecs[jobs[oldJob]["name"]]["type"] != newType:
+			jobs[oldJob]["crew"] = null
+			crew[crewID]["job"] = null
 
 
 func checkMen():
@@ -41,7 +149,6 @@ func checkMen():
 func requestCrew(itemRef, amount : int, taskGroup):
 	"""
 	When a cannon or something requests crew of type taskgroup, this function gets called.
-
 	itemRef: refernce to the item that requested the crew
 	amunt: how many men are needed
 	taskGroup: from which task group
@@ -49,7 +156,6 @@ func requestCrew(itemRef, amount : int, taskGroup):
 	# for i in range(amount)
 		# var bestMan = findBestMan(itemRef.translation, taskGroup)
 		# itemRef.assignMan(bestMan)
-
 	pass
 
 func findBestMen(itemPos : Vector3, taskGroup):
@@ -73,7 +179,6 @@ func groupItemsIntoChunks(items : Dictionary):
 	"""
 	groups item dictionary into chunks useable per crew member when item dictionary request is large.
 	one crew member can hold N items, so one chunk also contains N items, maybe even of the same type, so crew is walking optimal.
-
 	returns a new array with the chunks. 
 	eg: [{gunpowder:10}, #chunk1
 		{gunpowder:2, cannonballs:5}] #chunk 2
