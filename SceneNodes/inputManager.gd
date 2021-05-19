28f0df77334cd2b90de642c976e97c658b41df72
mayonnace <81087663+mayonnace@users.noreply.github.com>
2021-05-19 19:14:33 +0300

    Add files via upload

---
+++ SceneNodes/inputManager.gd
@@ -0,0 +1,44 @@
+extends Node
+
+
+var aiming: bool = false
+var firing: bool = false
+
+var leftClick: bool = false
+var rightClick: bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalObjectReferencer.inputManager = self
+
+
+# Catches only the input which has not been handled yet.
+func _unhandled_input(event):
+	if event.is_action_pressed("leftClick") && !event.is_echo():
+		leftClick = true
+	if event.is_action_pressed("rightClick") && !event.is_echo():
+		rightClick = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	firing = false
+	var mayFire = false
+	if aiming:
+		mayFire = true
+	if GlobalObjectReferencer.shopping.open != null:
+		aiming = false
+	elif Input.is_action_pressed("rightClick"):
+		aiming = true
+	else:
+		aiming = false
+	if mayFire && !aiming:
+		firing = true
+	reset()
+
+
+# Resets all inputs to false.
+func reset():
+	leftClick = false
+	rightClick = false
